806010b6f9e686629bcf901f5bd135bb97bc9eac
santibrizuela <santibrizuela.97@gmail.com>
2024-05-10 18:58:55 -0300

    BannerChevrolet changes again

--- app/components/bannerChevrolet/BannerChevrolet.tsx
+++ app/components/bannerChevrolet/BannerChevrolet.tsx
@@ -5,17 +5,9 @@ import { FaArrowRight } from "react-icons/fa";
 
 const HomeBanner = () => {
     return ( 
-        <div >
-            <div className="mx-auto flex flex-col gap-2 md:flex-row items-end justify-evenly bg-cover bg-center relative mb-4" style={{ backgroundImage: "url('../../../carouselchevrolet1.jpg')"}}>
-                <div className="mb-8 md:mb-0 text-center ease-in duration-300">
-                   
-                </div>
-                <div className="w-1/3 relative aspect-video flex flex-col justify-end">
-                    <Link className="flex justify-center text-yellow-400 home-banner-link ease-in duration-300" href="/plandeahorro">
-                        <p className="text-2xl md:text-5xl font-bold ease-in duration-300">120 Cuotas</p>
-                        <FaArrowRight className="ml-2 self-center md:text-[60px] align-middle ease-in duration-300" />
-                    </Link>
-                </div>
+        <div className="" >
+            <div className="mx-auto px-8 py-12 flex flex-col gap-2 items-end bg-cover bg-center h-[30vh] md:h-[50vh] lg:h-[70vh] relative mb-4" style={{ backgroundImage: "url('../../../carouselchevrolet1.jpg')"}}>
+                
             </div>
         </div>
      );
