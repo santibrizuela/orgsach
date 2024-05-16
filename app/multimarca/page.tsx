612a900234ec3b20540c5e9b2b34d2fddc59f0ff
santibrizuela <santibrizuela.97@gmail.com>
2024-05-16 15:50:33 -0300

    multimarcachages

--- app/multimarca/page.tsx
+++ app/multimarca/page.tsx
@@ -1,6 +1,6 @@
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import Container from "../components/Container";
-import { products } from "@/utils/products";
+import { usados } from "@/utils/usados";
 import ProductCard from "../components/products/ProductCard";
 import TopImage from "@/public/headerImg.png"
 import Image from "next/image";
@@ -20,7 +20,7 @@ const Multimarca = async () => {
             </div>
             <Container>
                 <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 mt-2">
-                    {products.map((product: any) => (
+                    {usados.map((product: any) => (
                         <ProductCard key={product.id} data={product} />
                     ))}
                 </div>
---
+++ utils/usados.tsx
@@ -0,0 +1,443 @@
+export const usados = [
+    {
+      id: "64a654593e91b8e73a351e9b",
+      name: "OROCH DYNAMIQUE 1.6",
+      description: "-Nafta<br/>-Manual<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
+      price: 21500000,
+      brand: "Renault",
+      category: "Usado",
+      year: 2019,
+      kms: 84500,
+      caja: "Manual",
+      combustible: "Nafta",
+      aireacondicionado: true,
+      cierrecentralizado: true,
+      cristaleselectricos: true,
+      alarma: true,
+      bluetooth: true,
+      inStock: true,
+      images: [
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/renaultdusterorochdynamique162019/renaultdusterorochdynamique1620191.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/renaultdusterorochdynamique162019/renaultdusterorochdynamique1620192.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/renaultdusterorochdynamique162019/renaultdusterorochdynamique1620193.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/renaultdusterorochdynamique162019/renaultdusterorochdynamique1620194.png",
+        },
+      ],
+      reviews: [],
+    },
+    {
+      id: "64a4ebe3qwe4bb50628a",
+      name: "CRONOS",
+      description:
+        "-Nafta<br/>-Automático<br/>-Aire acondicionado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth<br/>-Sensores de estacionamiento<br/>-Tapizado de cuero<br/>-Pantalla multimedia<br/>-Cámara de retroceso",
+      price: 23500000,
+      brand: "Fiat",
+      category: "0km",
+      year: 2024,
+      kms: 0,
+      inStock: true,
+      images: [
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/fiatcronosprecisionat132024/fiatcronosprecisionat1320241.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/fiatcronosprecisionat132024/fiatcronosprecisionat1320242.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/fiatcronosprecisionat132024/fiatcronosprecisionat1320243.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/fiatcronosprecisionat132024/fiatcronosprecisionat1320244.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/fiatcronosprecisionat132024/fiatcronosprecisionat1320245.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/fiatcronosprecisionat132024/fiatcronosprecisionat1320246.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/fiatcronosprecisionat132024/fiatcronosprecisionat1320247.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/fiatcronosprecisionat132024/fiatcronosprecisionat1320248.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/fiatcronosprecisionat132024/fiatcronosprecisionat1320249.png",
+        },
+      ],
+      reviews: [
+      ],
+    },
+    {
+      id: "64a654593e91b8e73a351e9c",
+      name: "325i",
+      description: "-Nafta<br/>-Manual<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
+      price: 19000,
+      brand: "BMW",
+      category: "Usado Premium",
+      year: 2010,
+      kms: 102400,
+      inStock: true,
+      images: [
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/bmw325i2010/bmw325i20101.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/bmw325i2010/bmw325i20102.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/bmw325i2010/bmw325i20103.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/bmw325i2010/bmw325i20104.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/bmw325i2010/bmw325i20105.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/bmw325i2010/bmw325i20106.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/bmw325i2010/bmw325i20107.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/bmw325i2010/bmw325i20108.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/bmw325i2010/bmw325i20109.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/bmw325i2010/bmw325i201010.png",
+        },
+      ],
+      reviews: [],
+    },
+    {
+      id: "65a654593e91b8e73a351e9c",
+      name: "A5 2.0 TURBO",
+      description: "-Nafta<br/>-Automático<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
+      price: 22300000,
+      brand: "Audi",
+      category: "Usado Premium",
+      year: 2012,
+      kms: 84500,
+      inStock: true,
+      images: [
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/audia520turbo2012/audia520turbo20121.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/audia520turbo2012/audia520turbo20122.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/audia520turbo2012/audia520turbo20123.png",
+        },
+        {
+          color: "Gray",
+          colorCode: "#808080",
+          image:
+            "/products/orgsachimages/audia520turbo2012/audia520turbo20124.png",
+        },
+      ],
+      reviews: [],
+    },
+    {
+      id: "65a654593e91b8aa351e9c",
+      name: "ECO SPORT SE",
+      description: "-Nafta<br/>-Manual<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
+      price: 11900000,
+      brand: "Ford",
+      category: "Usado",
+      year: 2013,
+      kms: 148000,
+      inStock: true,
+      images: [
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/fordecosportse162013/fordecosportse1620131.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/fordecosportse162013/fordecosportse1620132.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/fordecosportse162013/fordecosportse1620133.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/fordecosportse162013/fordecosportse1620134.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/fordecosportse162013/fordecosportse1620135.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/fordecosportse162013/fordecosportse1620136.png",
+        },
+      ],
+      reviews: [],
+    },
+    {
+      id: "65a654593e91b18e73q51e9c",
+      name: "PARTNER CONFORT 1.6",
+      description: "-Nafta<br/>-Manual<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Bluetooth",
+      price: 17900000,
+      brand: "Peugeot",
+      category: "Usado",
+      year: 2022,
+      kms: 126000,
+      inStock: true,
+      images: [
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/peugeotpartnerconfort162022/peugeotpartnerconfort1620221.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/peugeotpartnerconfort162022/peugeotpartnerconfort1620222.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/peugeotpartnerconfort162022/peugeotpartnerconfort1620223.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/peugeotpartnerconfort162022/peugeotpartnerconfort1620224.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/peugeotpartnerconfort162022/peugeotpartnerconfort1620225.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/peugeotpartnerconfort162022/peugeotpartnerconfort1620226.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/peugeotpartnerconfort162022/peugeotpartnerconfort1620227.png",
+        },
+      ],
+      reviews: [],
+    },
+    {
+      id: "648437b38c44d52b9542e340",
+      name: "GOL TREND SD BASE",
+      description:
+        '-Nafta<br/>-Manual<br/>-Aire acondicionado<br/>-Alarma<br/>-Bluetooth',
+      price: 10900000,
+      brand: "Volkswagen",
+      category: "Usado",
+      year: 2017,
+      kms: 63000,
+      inStock: true,
+      images: [
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/vwgoltrendsdbase2017/vwgoltrendsdbase20171.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/vwgoltrendsdbase2017/vwgoltrendsdbase20172.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/vwgoltrendsdbase2017/vwgoltrendsdbase20173.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/vwgoltrendsdbase2017/vwgoltrendsdbase20174.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/vwgoltrendsdbase2017/vwgoltrendsdbase20175.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/vwgoltrendsdbase2017/vwgoltrendsdbase20176.png",
+        },
+        {
+          color: "Black",
+          colorCode: "#000000",
+          image:
+            "/products/orgsachimages/vwgoltrendsdbase2017/vwgoltrendsdbase20177.png",
+        },
+      ],
+      reviews: [
+      ],
+    },
+    {
+      id: "65a654593e91b8f73a351e9c",
+      name: "CORSA CLASSIC LTS SPIRIT 1.4N",
+      description: "-Nafta / Gas<br/>-Manual<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos",
+      price: 7500000,
+      brand: "Chevrolet",
+      category: "Usado",
+      year: 2012,
+      kms: 138000,
+      inStock: true,
+      images: [
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/chevroletcorsaclassic4ptasltspirit14n2012/chevroletcorsaclassic4ptasltspirit14n20121.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/chevroletcorsaclassic4ptasltspirit14n2012/chevroletcorsaclassic4ptasltspirit14n20122.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/chevroletcorsaclassic4ptasltspirit14n2012/chevroletcorsaclassic4ptasltspirit14n20123.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/chevroletcorsaclassic4ptasltspirit14n2012/chevroletcorsaclassic4ptasltspirit14n20124.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/chevroletcorsaclassic4ptasltspirit14n2012/chevroletcorsaclassic4ptasltspirit14n20125.png",
+        },
+        {
+          color: "White",
+          colorCode: "#FFFFFF",
+          image:
+            "/products/orgsachimages/chevroletcorsaclassic4ptasltspirit14n2012/chevroletcorsaclassic4ptasltspirit14n20126.png",
+        },
+      ],
+      reviews: [],
+    },
+  ];
