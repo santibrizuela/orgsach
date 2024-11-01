4976d5d8bfbfafd288f5fbbd54b83222d106ccc4
santibrizuela <santibrizuela.97@gmail.com>
2024-11-01 19:14:55 -0300

    multimarcadesciendepda

--- utils/usados.tsx
+++ utils/usados.tsx
@@ -1,233 +1,5 @@
 export const usados = [
   {
-    id: "65a654593eg73a351e9c",
-    name: "TRACKER 1.2 MT",
-    description: "-Nafta<br/>-Manual<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
-    price: 245133,
-    brand: "Chevrolet",
-    category: "Plan de ahorro",
-    year: 2024,
-    kms: 0,
-    inStock: true,
-    images: [
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/chevrolettracker12mt/tracker12mt1.png",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/chevrolettracker12mt/tracker12mt2.png",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/chevrolettracker12mt/tracker12mt3.png",
-      },
-    ],
-    reviews: [],
-  },
-  {
-    id: "65a654593eg73a462e9c",
-    name: "TRACKER 1.2 AT",
-    description: "-Nafta<br/>-Automático<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
-    price: 287032,
-    brand: "Chevrolet",
-    category: "Plan de ahorro",
-    year: 2024,
-    kms: 0,
-    inStock: true,
-    images: [
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat1.png",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat2.png",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat3.png",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat4.png",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat5.png",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat6.png",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat7.png",
-      },
-    ],
-    reviews: [],
-  },
-  {
-    id: "65a654593e92b8e73a351e9c",
-    name: "ONIX 1.0 LT MT",
-    description: "-Nafta<br/>-Manual<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
-    price: 218618,
-    brand: "Chevrolet",
-    category: "Plan de ahorro",
-    year: 2024,
-    kms: 0,
-    inStock: true,
-    images: [
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/onix12/onix121.png",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/onix12/onix122.png",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/onix12/onix123.png",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/onix12/onix124.png",
-      },
-    ],
-    reviews: [],
-  },
-  {
-    id: "65a654593e9a73a351e9c",
-    name: "S10 CD 2.8 4x2 LS MT",
-    description: "-Nafta<br/>-Manual<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
-    price: 282180,
-    brand: "Chevrolet",
-    category: "Plan de ahorro",
-    year: 2024,
-    kms: 0,
-    inStock: true,
-    images: [
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/s10cd4x2lsmt/s10cd4x2lsmt1.jpg",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/s10cd4x2lsmt/s10cd4x2lsmt2.jpg",
-      },
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/s10cd4x2lsmt/s10cd4x2lsmt3.png",
-      },
-    ],
-    reviews: [],
-  },
-  {
-    id: "65a654593e91b8e73a351e9p",
-    name: "CRUZE PREMIER",
-    description: "-Nafta<br/>-Automático<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
-    price: 36900000,
-    brand: "Chevrolet",
-    category: "0km",
-    year: 2024,
-    kms: 0,
-    inStock: true,
-    images: [
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/chevroletcruzepremier2024/chevroletcruzepremier20241.png",
-      },
-      {
-        color: "Gray",
-        colorCode: "#808080",
-        image:
-          "/products/orgsachimages/chevroletcruzepremier2024/chevroletcruzepremier20242.png",
-      },
-      {
-        color: "Gray",
-        colorCode: "#808080",
-        image:
-          "/products/orgsachimages/chevroletcruzepremier2024/chevroletcruzepremier20243.png",
-      },
-      {
-        color: "Gray",
-        colorCode: "#808080",
-        image:
-          "/products/orgsachimages/chevroletcruzepremier2024/chevroletcruzepremier20244.png",
-      },
-      {
-        color: "Gray",
-        colorCode: "#808080",
-        image:
-          "/products/orgsachimages/chevroletcruzepremier2024/chevroletcruzepremier20245.png",
-      },
-      {
-        color: "Gray",
-        colorCode: "#808080",
-        image:
-          "/products/orgsachimages/chevroletcruzepremier2024/chevroletcruzepremier20246.png",
-      },
-    ],
-    reviews: [],
-  },
-  {
-    id: "65ama243e91b8e73a351e9p",
-    name: "AMAROK V6",
-    description: "-Nafta<br/>-Automático<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
-    price: 78000000,
-    brand: "VOLKSWAGEN",
-    category: "0km",
-    year: 2024,
-    kms: 0,
-    inStock: true,
-    images: [
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-          "/products/orgsachimages/volkswagenamarokv62024/volkswagenamarokv620241.png",
-      },
-    ],
-    reviews: [],
-  },
-  {
     id: "volkswagenamarokv6comfortlie3_0ltdi4x42019",
     name: "AMAROK V6",
     description: "-Nafta<br/>-Automática<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma",
@@ -855,6 +627,234 @@ export const usados = [
         colorCode: "#FFFFFF",
         image:
           "https://drive.google.com/uc?export=view&id=1giSGyyx8Pdack6zIQA-SkEX2cxgMJxiL",
+      },
+    ],
+    reviews: [],
+  },
+  {
+    id: "65a654593eg73a351e9c",
+    name: "TRACKER 1.2 MT",
+    description: "-Nafta<br/>-Manual<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
+    price: 245133,
+    brand: "Chevrolet",
+    category: "Plan de ahorro",
+    year: 2024,
+    kms: 0,
+    inStock: true,
+    images: [
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/chevrolettracker12mt/tracker12mt1.png",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/chevrolettracker12mt/tracker12mt2.png",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/chevrolettracker12mt/tracker12mt3.png",
+      },
+    ],
+    reviews: [],
+  },
+  {
+    id: "65a654593eg73a462e9c",
+    name: "TRACKER 1.2 AT",
+    description: "-Nafta<br/>-Automático<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
+    price: 287032,
+    brand: "Chevrolet",
+    category: "Plan de ahorro",
+    year: 2024,
+    kms: 0,
+    inStock: true,
+    images: [
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat1.png",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat2.png",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat3.png",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat4.png",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat5.png",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat6.png",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/chevrolettracker12ltat/chevrolettracker12ltat7.png",
+      },
+    ],
+    reviews: [],
+  },
+  {
+    id: "65a654593e92b8e73a351e9c",
+    name: "ONIX 1.0 LT MT",
+    description: "-Nafta<br/>-Manual<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
+    price: 218618,
+    brand: "Chevrolet",
+    category: "Plan de ahorro",
+    year: 2024,
+    kms: 0,
+    inStock: true,
+    images: [
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/onix12/onix121.png",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/onix12/onix122.png",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/onix12/onix123.png",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/onix12/onix124.png",
+      },
+    ],
+    reviews: [],
+  },
+  {
+    id: "65a654593e9a73a351e9c",
+    name: "S10 CD 2.8 4x2 LS MT",
+    description: "-Nafta<br/>-Manual<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
+    price: 282180,
+    brand: "Chevrolet",
+    category: "Plan de ahorro",
+    year: 2024,
+    kms: 0,
+    inStock: true,
+    images: [
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/s10cd4x2lsmt/s10cd4x2lsmt1.jpg",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/s10cd4x2lsmt/s10cd4x2lsmt2.jpg",
+      },
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/s10cd4x2lsmt/s10cd4x2lsmt3.png",
+      },
+    ],
+    reviews: [],
+  },
+  {
+    id: "65a654593e91b8e73a351e9p",
+    name: "CRUZE PREMIER",
+    description: "-Nafta<br/>-Automático<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
+    price: 36900000,
+    brand: "Chevrolet",
+    category: "0km",
+    year: 2024,
+    kms: 0,
+    inStock: true,
+    images: [
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/chevroletcruzepremier2024/chevroletcruzepremier20241.png",
+      },
+      {
+        color: "Gray",
+        colorCode: "#808080",
+        image:
+          "/products/orgsachimages/chevroletcruzepremier2024/chevroletcruzepremier20242.png",
+      },
+      {
+        color: "Gray",
+        colorCode: "#808080",
+        image:
+          "/products/orgsachimages/chevroletcruzepremier2024/chevroletcruzepremier20243.png",
+      },
+      {
+        color: "Gray",
+        colorCode: "#808080",
+        image:
+          "/products/orgsachimages/chevroletcruzepremier2024/chevroletcruzepremier20244.png",
+      },
+      {
+        color: "Gray",
+        colorCode: "#808080",
+        image:
+          "/products/orgsachimages/chevroletcruzepremier2024/chevroletcruzepremier20245.png",
+      },
+      {
+        color: "Gray",
+        colorCode: "#808080",
+        image:
+          "/products/orgsachimages/chevroletcruzepremier2024/chevroletcruzepremier20246.png",
+      },
+    ],
+    reviews: [],
+  },
+  {
+    id: "65ama243e91b8e73a351e9p",
+    name: "AMAROK V6",
+    description: "-Nafta<br/>-Automático<br/>-Aire acondicionado<br/>-Cierre centralizado<br/>-Cristales eléctricos<br/>-Alarma<br/>-Bluetooth",
+    price: 78000000,
+    brand: "VOLKSWAGEN",
+    category: "0km",
+    year: 2024,
+    kms: 0,
+    inStock: true,
+    images: [
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "/products/orgsachimages/volkswagenamarokv62024/volkswagenamarokv620241.png",
       },
     ],
     reviews: [],
